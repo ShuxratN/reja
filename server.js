ea4f2bdc8d4b9987ef093aecf3bfeb70e05c2508
ShuxratN <shuxratbeknozimjonov12@gmail.com>
2025-02-21 15:39:37 +0900

    feat: develop author router ejs page

--- server.js
+++ server.js
@@ -2,6 +2,21 @@ console.log("Web Serverni Boshlash");
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if(err) {
+        console.log("ERROR:", err);
+    } else {
+        user = JSON.parse(data)
+    }
+});
+
+
+
+
+
 
 //1 KIRISH CODE
 app.use(express.static("public"));
@@ -15,13 +30,20 @@ app.set("views", "views");
 app.set("view engine","ejs");
 
 //4 ROUTING CODE
-app.get("/", function (req, res) {
-    res.end(`<h1 style="background:red">HELLO WORLD by little prince</h1>`);
-});
-app.get("/gift", function (req, res) {
-    res.end(`<h1> Siz sovgalar bolimidasiz</h1>`);
+
+
+app.post("/create-item", (req, res) => {
+    //console.log(req.body);
+    //res.json({ test: "success" });
 });
 
+app.get("/author", (req, res) => {
+    res.render("author", { user: user});
+}); 
+
+app.get("/", function (req, res) {
+    res.render("harid");
+}); 
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function () {
