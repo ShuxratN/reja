529529774818638dd990b6dcd88665ab40cc4253
ShuxratN <shuxratbeknozimjonov12@gmail.com>
2025-02-24 20:26:42 +0900

    train.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-console.log("Web Serverni Boshlash");
+//console.log("Web Serverni Boshlash");
 const express = require("express");
 const app = express();
 const http = require("http");
@@ -13,40 +13,31 @@ fs.readFile("database/user.json", "utf8", (err, data) => {
     }
 });
 
-
-
-
-
-
 //1 KIRISH CODE
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 //2 Session CODE
-
 //3  VIEWS CODE
 app.set("views", "views");
 app.set("view engine","ejs");
 
 //4 ROUTING CODE
-
-
 app.post("/create-item", (req, res) => {
     //console.log(req.body);
     //res.json({ test: "success" });
 });
-
 app.get("/author", (req, res) => {
     res.render("author", { user: user});
-}); 
+});  
 
 app.get("/", function (req, res) {
-    res.render("harid");
+   res.render("harid");
 }); 
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function () {
     console.log(`the server is running succesfully on port: ${PORT}`);
 
-});
+}); 
--- train .js
+++ train .js
@@ -1,3 +1,48 @@
-console.log('train task ishga tushdi');
+//console.log('train task ishga tushdi');
+console.log ('Jack Ma maslahtlari');
+const list = [
+    "yahshi talaba boling", // 0-20
+    "togri boshliq tanlang va koproq hato qiling", // 20-30
+    "uzingizga ishlashingizni boshlang", // 30-40
+    "siz kuchli bolgan narsalarni qiling", // 40-50
+    "yoshlarga investitsiya qiling", // 50-60
+    "endi dam oling, foydasi yoq endi", // 60
+    ];
 
+    /*function maslahatBering(a, callback) {
+        if(typeof a !== 'number') callback("insert a number", null) ;
+        else if (a <= 20) callback (null, list[0]);
+        else if (a > 20 && a <= 30) callback(null, list[1]);
+        else if (a > 30 && a <= 40) callback(null, list[2]);
+        else if (a > 40 && a <= 50) callback(null, list[3]);
+        else if (a > 50 && a <= 60) callback(null, list[4]);
+        else{
+            setTimeout(function() {
+            callback(null, list[5]);
+            },5000);
+        }
+    }
+    maslahatBering(10, (err, data) => {
+        if(err) console.log('ERROR:', err);
 
+        console.log('javob:', data);
+    } ); 
+    console.log("passed here1");*/
+
+    function maslahatBering(a, callback) {
+        if(typeof a !== 'number') throw new Error("insert a number") ;
+        else if (a <= 20) return (null, list[0]);
+        else if (a > 20 && a <= 30) return(null, list[1]);
+        else if (a > 30 && a <= 40) return(null, list[2]);
+        else if (a > 40 && a <= 50) return(null, list[3]);
+        else if (a > 50 && a <= 60) return(null, list[4]);
+        else{
+            return list[5];
+           // setTimeout(function() {
+           // return list[5];
+           // },5000);
+        }
+    }
+
+    console.log("passed here 0");
+    maslahatBering(10, 25);
