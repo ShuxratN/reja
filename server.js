fe32c0e41cf51b85cc05ad790c63e558541644a5
ShuxratN <shuxratbeknozimjonov12@gmail.com>
2025-02-26 18:36:14 +0900

    feat: pushung

--- server.js
+++ server.js
@@ -38,6 +38,8 @@ app.get("/", function (req, res) {
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function () {
-    console.log(`the server is running succesfully on port: ${PORT}`);
+    console.log(
+        `The server is running succesfully on port: ${PORT}, http://localhost:${PORT}`
+    );
  
 }); 
