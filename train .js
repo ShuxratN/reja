e8f35142dfe3f3a50b8793e2421a47b8f8ddb11e
ShuxratN <shuxratbeknozimjonov12@gmail.com>
2025-03-05 18:27:41 +0900

    feat: d task

--- train .js
+++ train .js
@@ -6,19 +6,150 @@ Shunday function tuzing, u 2ta string parametr ega bolsin, hamda agar har ikkala
 MASALAN checkContent("mitgroup", "gmtiprou") return qiladi true;*/
 
 
- class StringCheck {
-    constructor(string1,  string2) {
-        this.string1 = this.string2;
-    }
-    
-    sameLetter (){
-        if (this.string1.lenght !== this.string2.lenght) return false;
-    }
- }
-console.log( sameLetter("hello", "hello"));
 
+
+
+
+
+
+
+
+
+
+
+
+/*
+D-TASK: 
+
+Shunday class tuzing tuzing nomi Shop, va uni constructoriga 3 hil mahsulot 
+pass bolsin, hamda classning 3ta methodi bolsin, 
+biri qoldiq, biri sotish va biri qabul. Har bir method ishga tushgan vaqt ham log qilinsin.
+MASALAN: const shop = new Shop(4, 5, 2); shop.qoldiq() return hozir 20:40da 4ta non,
+ 5ta lagmon va 2ta cola mavjud! shop.sotish('non', 3) & shop.qabul
+ ('cola', 4) & shop.qoldiq() return hozir 20:50da 1ta non, 5ta lagmon va 6ta cola mavjud!*/
+
+ class Shop {
+    constructor(non, lagmon, cola) {
+      this.non = non;
+      this.lagmon = lagmon;
+      this.cola = cola;
+    }
+  
+   
+    qoldiq() {
+      console.log(`Hozirda ${this.non}ta non, ${this.lagmon}ta lagmon va ${this.cola}ta cola mavjud!`);
+    }
+  
+    sotish(product, soni) {
+      if (product === 'non') this.non -= soni;
+      if (product === 'lagmon') this.lagmon -= soni;
+      if (product === 'cola') this.cola -= soni;
+      console.log(` ${soni} ta, ${product} sotildi.`);
+    }
+  
+    qabul(product, soni) {
+      if (product === 'non') this.non += soni;
+      if (product === 'lagmon') this.lagmon += soni;
+      if (product === 'cola') this.cola += soni;
+      console.log(` ${soni} ta, ${product} qabul qilindi.`);
+    }
+  }
+  const shop = new Shop(4, 5, 2);
+  shop.qoldiq();  
+  shop.sotish('non', 3);  
+  shop.qabul('cola', 4);   
+    
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+class StringChecker {
+    constructor(str1, str2) {
+      this.str1 = str1;
+      this.str2 = str2;
+    }
+  
+    // Method to check if both strings consist of the same letters
+    hasSameLetters() {
+      // If lengths are different, return false
+      if (this.str1.length !== this.str2.length) return false;
+    }
+  }
+  
+  // Function to check and return result
+  function checkContent(str1, str2) {
+    const checker = new StringChecker(str1, str2);
+    return checker.hasSameLetters();
+  }
+  
+  // Test Cases
+  console.log(checkContent("mitgroup", "gmtiprou")); // true
+  */
 
   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -113,5 +244,4 @@ const list = [
         javob = await maslahatBering (41);
         console.log( javob);
     }
-    run();
-*/  
+    run();*/ 
